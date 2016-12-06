ddd5ad888bf4068ebd61276ccffb48362a061ae1
Ashley Scott <admin@projectge.com>
2016-12-06 23:09:33 +0000

    Refactors code and adds the general structure for tilemaps to be loaded in

--- js/game/main.js
+++ js/game/main.js
@@ -1,9 +1,12 @@
-var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.CANVAS, 'Hack and Slash', { preload: preload, create: create, update: update, render: render });
+var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.CANVAS, 'Hack and Slash', { preload: GamePreload, create: GameCreate, update: GameUpdate, render: GameRender });
 var player;
 
 var vkUpIsDown, vkDownIsDown, vkLeftIsDown, vkRightIsDown = false;
 
-function preload()
+/**
+ * Preloads all assets.
+ */
+function GamePreload()
 {
 	game.load.image('background','assets/spr_ground.png');
 	game.load.image('player','assets/spr_player.png');
@@ -16,86 +19,58 @@ function preload()
 	game.load.image('gamepad_left','assets/ui/controller/gamepad_left.png');
 	game.load.image('gamepad_right','assets/ui/controller/gamepad_right.png');
 
-	game.load.tilemap('MainWorld_json_file', 'assets/maps/data/MainWorld.json', null, Phaser.Tilemap.TILED_JSON);
-	game.load.image('MainWorld_tiles_file', 'assets/maps/tiles/Outside_A2.png');
+	game.load.tilemap('MainWorld_JSON', 'assets/maps/data/MainWorld.json', null, Phaser.Tilemap.TILED_JSON);
+	game.load.image('MainWorld_TILES', 'assets/maps/tiles/MainSet.png');
 }
 
-function create()
+/**
+ * Called once on start.
+ */
+function GameCreate()
 {
-	game.world.setBounds(0, 0, 1920, 1080);
+	game.world.setBounds(0, 0, 0, 0);
 	game.physics.startSystem(Phaser.Physics.P2JS);
 
-	map = game.add.tilemap('MainWorld_json_file');
-	map.addTilesetImage('Outside_A2', 'MainWorld_tiles_file');
-	layer = map.createLayer('MainWorld_layer01');
-	layer.resizeWorld();
+	AddWorldLayer('MainWorld', 'MainSet');
 
-	player = game.add.sprite(game.world.centerX, game.world.centerY, 'player');
-	game.physics.p2.enable(player);
-	player.body.fixedRotation = true;
+	SetupPlayer('player', false);
 
-	upKey = game.input.keyboard.addKey(Phaser.Keyboard.W);
-	downKey = game.input.keyboard.addKey(Phaser.Keyboard.S);
-	leftKey = game.input.keyboard.addKey(Phaser.Keyboard.A);
-	rightKey = game.input.keyboard.addKey(Phaser.Keyboard.D);
+	SetupInterface();
 
-	// DEBUG ONLY
-	healthUp = game.input.keyboard.addKey(Phaser.Keyboard.U);
-	healthDown = game.input.keyboard.addKey(Phaser.Keyboard.J);
-
-	menuLabel = game.add.text(0, 0, 'MENU', { font: '18px Arial', fill: '#FFFFFF' });
-	menuLabel.inputEnabled = true;
-	menuLabel.events.onInputUp.add(ToggleMenu, self);
-
-	healthBar = game.add.image(10, 10, "health_bar");
-	healthBar.height = 16;
-	healthBar.width = 300;
-	healthBar.initialWidth = 300;
-
-	debugLabel = game.add.text(0, 0, 'DEBUG', { font: '14px Arial', fill: 'red' });
-
-	gamepadBack = game.add.image(16, window.innerHeight-144, "gamepad_back");
-	gamepadBack.fixedToCamera = true;
-
-	gamepadUp = game.add.button(80, window.innerHeight-112, "gamepad_up", null, this, 0, 1, 0, 1);
-    gamepadUp.events.onInputOver.add(function(){vkUpIsDown = true;});
-    gamepadUp.events.onInputOut.add(function(){vkUpIsDown = false;});
-    gamepadUp.events.onInputDown.add(function(){vkUpIsDown = true;});
-    gamepadUp.events.onInputUp.add(function(){vkUpIsDown = false;});
-	gamepadUp.fixedToCamera = true;
-    gamepadUp.anchor.setTo(0.5, 0.5);
-
-	gamepadDown = game.add.button(80, window.innerHeight-48, "gamepad_down", null, this, 0, 1, 0, 1);
-    gamepadDown.events.onInputOver.add(function(){vkDownIsDown = true;});
-    gamepadDown.events.onInputOut.add(function(){vkDownIsDown = false;});
-    gamepadDown.events.onInputDown.add(function(){vkDownIsDown = true;});
-    gamepadDown.events.onInputUp.add(function(){vkDownIsDown = false;});
-	gamepadDown.fixedToCamera = true;
-    gamepadDown.anchor.setTo(0.5, 0.5);
-
-	gamepadLeft = game.add.button(48, window.innerHeight-80, "gamepad_left", null, this, 0, 1, 0, 1);
-    gamepadLeft.events.onInputOver.add(function(){vkLeftIsDown = true;});
-    gamepadLeft.events.onInputOut.add(function(){vkLeftIsDown = false;});
-    gamepadLeft.events.onInputDown.add(function(){vkLeftIsDown = true;});
-    gamepadLeft.events.onInputUp.add(function(){vkLeftIsDown = false;});
-	gamepadLeft.fixedToCamera = true;
-    gamepadLeft.anchor.setTo(0.5, 0.5);
-
-	gamepadRight = game.add.button(112, window.innerHeight-80, "gamepad_right", null, this, 0, 1, 0, 1);
-    gamepadRight.events.onInputOver.add(function(){vkRightIsDown = true;});
-    gamepadRight.events.onInputOut.add(function(){vkRightIsDown = false;});
-    gamepadRight.events.onInputDown.add(function(){vkRightIsDown = true;});
-    gamepadRight.events.onInputUp.add(function(){vkRightIsDown = false;});
-	gamepadRight.fixedToCamera = true;
-    gamepadRight.anchor.setTo(0.5, 0.5);
-
-	game.camera.follow(player, Phaser.Camera.FOLLOW_LOCKON, 0.1, 0.1);
+	SetupGameControls();
+	SetupVirtualGameControls();
 }
 
-function update()
+/**
+ * Called every frame.
+ */
+function GameUpdate()
 {
 	HandleInput();
+	DebugUpdate();
+}
 
+/**
+ * Called every frame.
+ */
+function GameRender()
+{
+	menuLabel.x = game.camera.x + 10;
+	menuLabel.y = game.camera.y + 28;
+
+	healthBar.x = game.camera.x + 10;
+	healthBar.y = game.camera.y + 10;
+	healthBar.width = player.health / player.maxHealth * healthBar.initialWidth;
+
+	debugLabel.x = game.camera.x + 10;
+	debugLabel.y = game.camera.y + 90;
+}
+
+/**
+ * Debug information.
+ */
+function DebugUpdate()
+{
 	debugLabel.text =   
 		"Screen Size: [w: " + window.innerWidth + ", h: " + window.innerHeight + "]\n" +
 		"Player Location: [x: " + parseInt(player.x, 10) + ", y: " + parseInt(player.y, 10) + "]\n" + 
@@ -105,22 +80,111 @@ function update()
 		"Health: [U, J] [Increment, Decrement]"
 }
 
-function render()
+/**
+ * Initialises the game with a world TileMap.
+ */
+function AddWorldLayer(worldId, tileSet)
 {
-	// I can use .fixedToCamera, but the lerp give it a nice moving effect.
-	// If combined with camera shake, it should look amazing.
-	menuLabel.x = game.camera.x + 10;
-	menuLabel.y = game.camera.y + 28;
+	map = game.add.tilemap(worldId + '_JSON');
+	map.addTilesetImage(tileSet, worldId + '_TILES');
 
-	healthBar.x = game.camera.x + 10;
-	healthBar.y = game.camera.y + 10;
-	healthBar.width = player.health / player.maxHealth * healthBar.initialWidth;
+	layer = map.createLayer('layer_' + worldId + '_Ground');
+	layer.resizeWorld();
 
-	// Debug
-	debugLabel.x = game.camera.x + 10;
-	debugLabel.y = game.camera.y + 90;
+	layer = map.createLayer('layer_' + worldId + '_Buildings');
+	layer.resizeWorld();
+
+	layer = map.createLayer('layer_' + worldId + '_Doors');
+	layer.resizeWorld();
+}
+/**
+ * Configured the player instance and locks the camera to it.
+ */
+function SetupPlayer(itemId, fixedRot)
+{
+	player = game.add.sprite(game.world.centerX, game.world.centerY, itemId);
+	
+	game.physics.p2.enable(player);
+	
+	player.body.fixedRotation = fixedRot;
+
+	game.camera.follow(player, Phaser.Camera.FOLLOW_LOCKON, 0.1, 0.1);
 }
 
+/**
+ * Configured the UI which is displayed in the game view.
+ */
+function SetupInterface()
+{
+	menuLabel = game.add.text(0, 0, 'MENU', { font: '18px Arial', fill: '#FFFFFF' });
+	menuLabel.inputEnabled = true;
+	menuLabel.events.onInputUp.add(MenuToggle, self);
+	
+	debugLabel = game.add.text(0, 0, 'DEBUG', { font: '14px Arial', fill: 'red' });
+
+	healthBar = game.add.image(10, 10, "health_bar");
+	healthBar.height = 16;
+	healthBar.width = 300;
+	healthBar.initialWidth = 300;
+}
+
+/**
+ * Configures the keyboard buttons that the game uses.
+ */
+function SetupGameControls()
+{
+	upKey = game.input.keyboard.addKey(Phaser.Keyboard.W);
+	downKey = game.input.keyboard.addKey(Phaser.Keyboard.S);
+	leftKey = game.input.keyboard.addKey(Phaser.Keyboard.A);
+	rightKey = game.input.keyboard.addKey(Phaser.Keyboard.D);
+	healthUp = game.input.keyboard.addKey(Phaser.Keyboard.U);
+	healthDown = game.input.keyboard.addKey(Phaser.Keyboard.J);
+}
+
+/**
+ * Configures the game's virtual control pad.
+ */
+function SetupVirtualGameControls()
+{
+	gamepadBack = game.add.image(16, window.innerHeight-144, "gamepad_back");
+	gamepadBack.fixedToCamera = true;
+
+	gamepadUp = game.add.button(80, window.innerHeight-112, "gamepad_up", null, this, 0, 1, 0, 1);
+	gamepadUp.events.onInputOver.add(function(){vkUpIsDown = true;});
+	gamepadUp.events.onInputOut.add(function(){vkUpIsDown = false;});
+	gamepadUp.events.onInputDown.add(function(){vkUpIsDown = true;});
+	gamepadUp.events.onInputUp.add(function(){vkUpIsDown = false;});
+	gamepadUp.fixedToCamera = true;
+	gamepadUp.anchor.setTo(0.5, 0.5);
+
+	gamepadDown = game.add.button(80, window.innerHeight-48, "gamepad_down", null, this, 0, 1, 0, 1);
+	gamepadDown.events.onInputOver.add(function(){vkDownIsDown = true;});
+	gamepadDown.events.onInputOut.add(function(){vkDownIsDown = false;});
+	gamepadDown.events.onInputDown.add(function(){vkDownIsDown = true;});
+	gamepadDown.events.onInputUp.add(function(){vkDownIsDown = false;});
+	gamepadDown.fixedToCamera = true;
+	gamepadDown.anchor.setTo(0.5, 0.5);
+
+	gamepadLeft = game.add.button(48, window.innerHeight-80, "gamepad_left", null, this, 0, 1, 0, 1);
+	gamepadLeft.events.onInputOver.add(function(){vkLeftIsDown = true;});
+	gamepadLeft.events.onInputOut.add(function(){vkLeftIsDown = false;});
+	gamepadLeft.events.onInputDown.add(function(){vkLeftIsDown = true;});
+	gamepadLeft.events.onInputUp.add(function(){vkLeftIsDown = false;});
+	gamepadLeft.fixedToCamera = true;
+	gamepadLeft.anchor.setTo(0.5, 0.5);
+
+	gamepadRight = game.add.button(112, window.innerHeight-80, "gamepad_right", null, this, 0, 1, 0, 1);
+	gamepadRight.events.onInputOver.add(function(){vkRightIsDown = true;});
+	gamepadRight.events.onInputOut.add(function(){vkRightIsDown = false;});
+	gamepadRight.events.onInputDown.add(function(){vkRightIsDown = true;});
+	gamepadRight.events.onInputUp.add(function(){vkRightIsDown = false;});
+	gamepadRight.fixedToCamera = true;
+	gamepadRight.anchor.setTo(0.5, 0.5);
+}
+
+/**
+ * Handles all of the input the user sends to the game.
+ */
 function HandleInput()
 {
 	player.body.setZeroVelocity();
@@ -157,5 +221,22 @@ function HandleInput()
 		{
 			player.health--;
 		}
+	}
+}
+
+/**
+ * Toggles the pause menu which is handled by the DOM.
+ */
+function MenuToggle()
+{
+	if($('.pause-menu').css('display') == 'none')
+	{
+		game.paused = true;
+		$('.pause-menu').fadeIn();
+	}
+	else
+	{
+		game.paused = false;
+		$('.pause-menu').fadeOut();
 	}
 }
